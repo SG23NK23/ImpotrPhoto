9c11e1917fd843f8e1666d423b9437de0d413216
SG23NK23 <nikolya.kovalev.2002@mail.ru>
2022-08-01 22:00:25 +0300

    Добавьте файлы проекта.

---
+++ Импорт изображений/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Импорт_изображений
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Label photoLabel;
+            this.yACHTSDataSet = new Импорт_изображений.YACHTSDataSet();
+            this.yachtBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.yachtTableAdapter = new Импорт_изображений.YACHTSDataSetTableAdapters.YachtTableAdapter();
+            this.tableAdapterManager = new Импорт_изображений.YACHTSDataSetTableAdapters.TableAdapterManager();
+            this.yachtBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.yachtBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.photoPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            photoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.yACHTSDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yachtBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yachtBindingNavigator)).BeginInit();
+            this.yachtBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yACHTSDataSet
+            // 
+            this.yACHTSDataSet.DataSetName = "YACHTSDataSet";
+            this.yACHTSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // yachtBindingSource
+            // 
+            this.yachtBindingSource.DataMember = "Yacht";
+            this.yachtBindingSource.DataSource = this.yACHTSDataSet;
+            // 
+            // yachtTableAdapter
+            // 
+            this.yachtTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ClientsTableAdapter = null;
+            this.tableAdapterManager.SalesTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = Импорт_изображений.YACHTSDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.YachtColorTableAdapter = null;
+            this.tableAdapterManager.YachtTableAdapter = this.yachtTableAdapter;
+            this.tableAdapterManager.YachtTypeTableAdapter = null;
+            // 
+            // yachtBindingNavigator
+            // 
+            this.yachtBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.yachtBindingNavigator.BindingSource = this.yachtBindingSource;
+            this.yachtBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.yachtBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.yachtBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.yachtBindingNavigatorSaveItem});
+            this.yachtBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.yachtBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.yachtBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.yachtBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.yachtBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.yachtBindingNavigator.Name = "yachtBindingNavigator";
+            this.yachtBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.yachtBindingNavigator.Size = new System.Drawing.Size(800, 25);
+            this.yachtBindingNavigator.TabIndex = 0;
+            this.yachtBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 15);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // yachtBindingNavigatorSaveItem
+            // 
+            this.yachtBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.yachtBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("yachtBindingNavigatorSaveItem.Image")));
+            this.yachtBindingNavigatorSaveItem.Name = "yachtBindingNavigatorSaveItem";
+            this.yachtBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.yachtBindingNavigatorSaveItem.Text = "Сохранить данные";
+            this.yachtBindingNavigatorSaveItem.Click += new System.EventHandler(this.yachtBindingNavigatorSaveItem_Click);
+            // 
+            // photoLabel
+            // 
+            photoLabel.AutoSize = true;
+            photoLabel.Location = new System.Drawing.Point(3, 44);
+            photoLabel.Name = "photoLabel";
+            photoLabel.Size = new System.Drawing.Size(38, 13);
+            photoLabel.TabIndex = 1;
+            photoLabel.Text = "Photo:";
+            // 
+            // photoPictureBox
+            // 
+            this.photoPictureBox.DataBindings.Add(new System.Windows.Forms.Binding("Image", this.yachtBindingSource, "Photo", true));
+            this.photoPictureBox.Location = new System.Drawing.Point(47, 44);
+            this.photoPictureBox.Name = "photoPictureBox";
+            this.photoPictureBox.Size = new System.Drawing.Size(381, 331);
+            this.photoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.photoPictureBox.TabIndex = 2;
+            this.photoPictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Импорт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(photoLabel);
+            this.Controls.Add(this.photoPictureBox);
+            this.Controls.Add(this.yachtBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yACHTSDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yachtBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yachtBindingNavigator)).EndInit();
+            this.yachtBindingNavigator.ResumeLayout(false);
+            this.yachtBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.photoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private YACHTSDataSet yACHTSDataSet;
+        private System.Windows.Forms.BindingSource yachtBindingSource;
+        private YACHTSDataSetTableAdapters.YachtTableAdapter yachtTableAdapter;
+        private YACHTSDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator yachtBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton yachtBindingNavigatorSaveItem;
+        private System.Windows.Forms.PictureBox photoPictureBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Импорт изображений/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Импорт_изображений
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void yachtBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.yachtBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.yACHTSDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "yACHTSDataSet.Yacht". При необходимости она может быть перемещена или удалена.
+            this.yachtTableAdapter.Fill(this.yACHTSDataSet.Yacht);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] masimgname = Directory.GetFiles("Photo");
+            for(int i = 0;i<masimgname.Length;i++)
+            {
+                string fName = Path.GetFileNameWithoutExtension(masimgname[i].ToUpper());
+                yachtBindingSource.Filter = "ModelName='" + fName + "'";
+                if(yachtBindingSource.Count >0)
+                {
+                    photoPictureBox.Load(masimgname[i]);
+                    photoPictureBox.Refresh();
+                    yachtBindingSource.EndEdit();
+                }
+            }
+
+            this.yachtTableAdapter.Update(this.yACHTSDataSet.Yacht);   
+        }
+    }
+}
